288fe46cd7c2ee50d344daa2c36e6e43b072ea85
bilgeedev <jrrsenpai@gmail.com>
2026-02-12 11:11:10 +0800

    z

--- src/components/ui/MonthPickerInput.tsx
+++ src/components/ui/MonthPickerInput.tsx
@@ -2,6 +2,11 @@
 
 import React, { useMemo, useRef, useState, useEffect } from "react";
 import dayjs from "dayjs";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
 
 type CommonProps = {
   placeholder?: string;
